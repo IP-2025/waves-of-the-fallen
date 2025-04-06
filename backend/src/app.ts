e3357454ace914c409207d81ef8998a41baf2bb8
Paul <jaul.ponas@gmail.com>
2025-04-06 20:47:21 +0200

    test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import dotenv from 'dotenv';
+import express from 'express';
+import dotenv from "dotenv";
 import healthRouter from './routes/health';
 import authRouter from './routes/auth';
 
