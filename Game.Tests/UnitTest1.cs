6b13cea627822ce3122a23a32f181e13a3219eed
maxfriebertshauser <22frma1bif@hft-stuttgart.de>
2025-04-09 21:24:27 +0200

    feat: added Tests to Game

---
+++ Game.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Game.Tests
+{
+    public class DefaultTests
+    {
+        [Fact]
+        public void AlwaysPasses()
+        {
+            Assert.True(true);
+        }
+    }
+}
