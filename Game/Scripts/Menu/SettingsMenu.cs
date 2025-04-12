22ec4f7a311b165ccbf4e1a0883ee6fdb6579cb0
l3nny123 <120551935+l3nny123@users.noreply.github.com>
2025-04-12 14:10:15 +0200

    feat: added background music (only in settings)
    added backgorund music in settings scene wich can be adjusted with a slider (ONLY in settings scene)

--- Game/Scripts/Menu/SettingsMenu.cs
+++ Game/Scripts/Menu/SettingsMenu.cs
@@ -3,9 +3,27 @@ using System;
 
 public partial class SettingsMenu : Control
 {
+ private HSlider sliderMusic;
+ private int menuBackgroundMusicBusIndex;
+
+public override void _Ready()
+{
+	sliderMusic = GetNode<HSlider>("MarginContainer2/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer3/HSliderMusic");
+	menuBackgroundMusicBusIndex=AudioServer.GetBusIndex("MenuBackgroundMusicBus");
+	AudioServer.SetBusVolumeDb(menuBackgroundMusicBusIndex,Mathf.LinearToDb(0));
+	//GD.Print(AudioServer.GetBusVolumeDb(menuBackgroundMusicBusIndex).ToString("0.000"));
+	
+	
+}
   private void _on_button_back_settings_pressed()
   {
 	var scene = ResourceLoader.Load<PackedScene>("res://Scenes/Menu/mainMenu.tscn");
 	GetTree().ChangeSceneToPacked(scene);
+  }
+  private void _on_h_slider_music_value_changed(float volumeSlider)
+  { 
+	GD.Print(volumeSlider.ToString("0.00"));
+	AudioServer.SetBusVolumeDb(menuBackgroundMusicBusIndex,Mathf.LinearToDb(volumeSlider));
+	
   }
 }
