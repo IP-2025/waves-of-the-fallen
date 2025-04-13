04f8308b71b0a96f94fe4b82172fa77498d5a3c0
Paul <jaul.ponas@gmail.com>
2025-04-13 15:12:47 +0200

    missing commit
    Co-authored-by: maxfriebertshauser <22frma1bif@hft-stuttgart.de>

--- backend/test/registerTest.test.ts
+++ backend/test/registerTest.test.ts
@@ -1,5 +1,11 @@
 import request from 'supertest';
 import app from '../src/app'; // Adjust the path to your app file
+import { insertNewCred } from '../src/repositories/credentialsRepository';
+import { getPrismaClient } from '../src/libs';
+import { v4 as uuidv4 } from 'uuid';
+import { Credential } from '../src/types/databaseEntries';
+
+const prisma = getPrismaClient();
 
 describe('POST /auth/register', () => {
   it('should return 400 if username or password is missing', async () => {
@@ -13,11 +19,36 @@ describe('POST /auth/register', () => {
 
 describe('POST /auth/login', () => {
   it('should return 201 if user is registered properly', async () => {
-    const  response = await request(app)
-    .post('/api/v1/auth/register')
-    .send({ username: 'testUser', password: 'testPassword' });
+    const response = await request(app)
+      .post('/api/v1/auth/register')
+      .send({ username: 'testUser', password: 'testPassword' });
     expect(response.status).toBe(201);
-    expect(response.body).toEqual({message: 'User testUser registered'});
+    expect(response.body).toEqual({ message: 'User testUser registered' });
   });
+});
 
-})
+describe('Check insertNewUser', () => {
+  it('should insert a new user', async () => {
+    const email = 'maxmustermann@gmail.com';
+    const password = 'XYZ123';
+    const playerId = uuidv4();
+
+    const newUser = await insertNewCred({ player_id: playerId, email: email, password: password });
+
+    expect(newUser).toBeDefined();
+    expect(newUser.email).toBe(email);
+    expect(newUser.player_id).toBeDefined();
+    expect(newUser.created_at).toBeInstanceOf(Date);
+
+    const savedUser: Credential = await prisma.$executeRawUnsafe(`SELECT * FROM credentials WHERE email = '${email}'`);
+
+    expect(savedUser).toBeDefined();
+    expect(savedUser?.email).toBe(newUser.email);
+    expect(savedUser?.player_id).toBe(newUser.player_id);
+    expect(savedUser?.password).toBe(newUser.password);
+    expect(savedUser?.created_at).toBeInstanceOf(Date);
+    expect(savedUser?.created_at).toBe(newUser.created_at);
+
+  });
+});
+
