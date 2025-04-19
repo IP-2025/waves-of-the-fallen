6e18eddf3763470c54afb12ce47c30f26502ca3b
l3nny123 <120551935+l3nny123@users.noreply.github.com>
2025-04-19 17:08:24 +0200

    feat(ui): reworked charcter selection
    -if a character is not unlocked the icon is black and white
    -a character can be selected, the button of the selected character is marked red
    - buttons change name depending on them beeing unlocked or not

--- Game/Scripts/Menu/ButtonsCharacterSelection.cs
+++ Game/Scripts/Menu/ButtonsCharacterSelection.cs
@@ -4,9 +4,14 @@ using System;
 public partial class ButtonsCharacterSelection : Button
 {
 	private Charactermenu _controller;
+	private Shader _blackAndWhiteShader;
 	
 	 public override void _Ready()
 	{
+		if(_blackAndWhiteShader==null)
+		{
+			_blackAndWhiteShader=GD.Load<Shader>("res://Scenes/Menu/characterMenuIconShader.gdshader");
+		}
 		Node node=GetParent();
 		while (_controller==null&&node!=null){
 			if(node is Charactermenu)
@@ -16,6 +21,15 @@ public partial class ButtonsCharacterSelection : Button
 			}
 			node=node.GetParent();
 		}
+		
+		if(this.Name=="Button_Character6") //if Character is locked =true
+		{
+			var icon = GetNode<TextureRect>("TextureRect");
+			var material=new ShaderMaterial();
+			material.Shader=_blackAndWhiteShader;
+			icon.Material= material;
+		}
+		
 	}
 	
 	private void _on_pressed()
--- Game/Scripts/Menu/Charactermenu.cs
+++ Game/Scripts/Menu/Charactermenu.cs
@@ -4,22 +4,64 @@ using System;
 public partial class Charactermenu : Control
 {
 	private Label _labelCharacterName;
+	private Button _currentlySelectedCharacter;
+	private Button _oldSelectedCharacter;
+	private Button _ButtonUpgradeUnlock;
+	private Button _Button_Select;
 	
 	public override void _Ready()
 	{
 		_labelCharacterName=GetNode<Label>("%Label_SelectedCharacterName");
+		_ButtonUpgradeUnlock=GetNode<Button>("%Button_UpgradeUnlock");
+		_Button_Select=GetNode<Button>("%Button_Select");
 	}
 	private void _on_button_back_charactermenu_pressed()
 	{
 		var scene = ResourceLoader.Load<PackedScene>("res://Scenes/Menu/mainMenu.tscn");
 		GetTree().ChangeSceneToPacked(scene);
 	}
+	
 	public void _characterSelected(ButtonsCharacterSelection button)
 	{
-		if(button!=null&&_labelCharacterName!=null){
-		GD.Print(button.Name);
-		_labelCharacterName.Text=button.Name;
+		if(button!=null && _labelCharacterName!=null){
+			_labelCharacterName.Text=button.Name;
+			_oldSelectedCharacter=_currentlySelectedCharacter;
+			_currentlySelectedCharacter=button;
+			if(1==1){ //check if character is unlocken. locked=true
+				_ButtonUpgradeUnlock.Text="Unlock";
+				_Button_Select.Disabled=true;
+			}else{
+				_ButtonUpgradeUnlock.Text="Upgrade";
+			}
 		}
-		
 	}
+	
+	private void _on_button_select_pressed( )
+	{
+		if(_currentlySelectedCharacter!=null)
+		{
+			var style = new StyleBoxFlat();
+			style.BgColor = Color.Color8(0x4F,0x4F,0x4F);
+			style.BorderColor= new Color(1f,0f,0f);
+			style.SetBorderWidth(Side.Top,3);
+			style.SetBorderWidth(Side.Bottom,3);
+			style.SetBorderWidth(Side.Left,3);
+			style.SetBorderWidth(Side.Right,3);
+			_currentlySelectedCharacter.AddThemeStyleboxOverride("normal",style);
+			
+		if(_oldSelectedCharacter!=_currentlySelectedCharacter){
+		_resetButton(_oldSelectedCharacter);
+		 }
+		}
+	}
+	private void _resetButton(Button button){
+		if(button!=null){
+		var style = new StyleBoxFlat();
+		style.BgColor = Color.Color8(0x4F,0x4F,0x4F);
+		button.AddThemeStyleboxOverride("normal",style);
+		}
+	}
+		
+	
+	
 }
