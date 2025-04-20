54cfb6092e17cfd34f209aa566751001e9061f94
maxfriebertshauser <22frma1bif@hft-stuttgart.de>
2025-04-20 19:01:40 +0200

    feat(settings): add endpoint for setting player settings

--- backend/src/routes/protected.ts
+++ backend/src/routes/protected.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import { authenticationStep} from "../middleware/validateMiddleware";
+import { fetchSettings, setSettings } from '../controllers/settingsController';
+import { getSettingsByPlayerId } from '../repositories/settingsRepository';
 
 const protectedRouter = express.Router();
+
+//protectedRouter.post('/fetchSettings', fetchSettings )
+protectedRouter.post('/setSettings', authenticationStep, setSettings);
 
 protectedRouter.get('/', authenticationStep, (req, res) => {
     res.json({ authenticated: true })
