dbe569159d1fad5ccd1021c1e14f7aef26adc52f
maxfriebertshauser <22frma1bif@hft-stuttgart.de>
2025-04-21 11:02:25 +0200

    test(settings): refactor protected settings tests for improved clarity and error handling

--- backend/test/routes/protectedSettings.test.ts
+++ backend/test/routes/protectedSettings.test.ts
@@ -2,21 +2,20 @@ import request from 'supertest';
 import app from '../../src/app';
 
 const generateTestUser = () => {
-  const timestamp = Date.now();
   return {
-    username: `TestUser_${timestamp}`,
-    email: `testuser_${timestamp}@example.com`,
+    username: `TestUser`,
+    email: `testuser@example.com`,
     password: '123456',
   };
 };
 
 let validToken: string;
 let registeredPlayerId: string;
+const invalidToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZmFrZSIsImlhdCI6MH0.invalidsignature';
 
 beforeEach(async () => {
   const testUser = generateTestUser();
 
-  // Register new user
   const registerResponse = await request(app)
     .post('/api/v1/auth/register')
     .send(testUser);
@@ -24,7 +23,7 @@ beforeEach(async () => {
   expect(registerResponse.status).toBe(201); // Correct status for resource creation
   registeredPlayerId = registerResponse.body.player_id;
 
-  // Login with new user
+
   const loginResponse = await request(app)
     .post('/api/v1/auth/login')
     .send({
@@ -36,71 +35,66 @@ beforeEach(async () => {
   validToken = loginResponse.body.token;
 });
 
-describe('User Settings API', () => {
-  const invalidToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZmFrZSIsImlhdCI6MH0.invalidsignature';
 
-  describe('POST /setSettings', () => {
-    it('should insert settings successfully', async () => {
-      const settingsData = {
-        player_id: registeredPlayerId,
-        musicVolume: 0.5,
-        soundVolume: 0.2,
-      };
+describe('POST /setSettings', () => {
+  it('should insert settings successfully', async () => {
+    const settingsData = {
+      player_id: registeredPlayerId,
+      musicVolume: 0.5,
+      soundVolume: 0.2,
+    };
 
-      const response = await request(app)
-        .post('/api/v1/protected/setSettings')
-        .set('Authorization', `Bearer ${validToken}`)
-        .send(settingsData);
+    const response = await request(app)
+      .post('/api/v1/protected/setSettings')
+      .set('Authorization', `Bearer ${validToken}`)
+      .send(settingsData);
 
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual(settingsData);
-    });
-
-    it('should fail with invalid token', async () => {
-      const settingsData = {
-        player_id: registeredPlayerId,
-        musicVolume: 0.5,
-        soundVolume: 0.2,
-      };
-
-      const response = await request(app)
-        .post('/api/v1/protected/setSettings')
-        .set('Authorization', `Bearer ${invalidToken}`)
-        .send(settingsData);
-
-      expect(response.status).toBe(401);
-      expect(response.body).toEqual({
-        message: 'Unauthorized',
-        status: 'error',
-      });
-    });
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(settingsData);
   });
 
-  describe('POST /getSettings', () => {
-    it('should retrieve user settings successfully', async () => {
-      const settingsData = {
-        player_id: registeredPlayerId,
-        musicVolume: 0.5,
-        soundVolume: 0.2,
-      };
+  it('should fail with invalid token', async () => {
+    const settingsData = {
+      player_id: registeredPlayerId,
+      musicVolume: 0.5,
+      soundVolume: 0.2,
+    };
 
-      // First, set the settings
-      const responseSet = await request(app)
-        .post('/api/v1/protected/setSettings')
-        .set('Authorization', `Bearer ${validToken}`)
-        .send(settingsData);
+    const response = await request(app)
+      .post('/api/v1/protected/setSettings')
+      .set('Authorization', `Bearer ${invalidToken}`)
+      .send(settingsData);
 
-      expect(responseSet.status).toBe(200);
-
-      // Then, retrieve the settings
-      const responseGet = await request(app)
-        .post('/api/v1/protected/getSettings')
-        .set('Authorization', `Bearer ${validToken}`)
-        .send({ player_id: registeredPlayerId });
-
-      expect(responseGet.status).toBe(200);
-      expect(responseGet.body).toEqual(settingsData);
+    expect(response.status).toBe(401);
+    expect(response.body).toEqual({
+      message: 'Unauthorized',
+      status: 'error',
     });
   });
 });
+
+describe('POST /getSettings', () => {
+  it('should retrieve user settings successfully', async () => {
+    const settingsData = {
+      player_id: registeredPlayerId,
+      musicVolume: 0.5,
+      soundVolume: 0.2,
+    };
+
+    const responseSet = await request(app)
+      .post('/api/v1/protected/setSettings')
+      .set('Authorization', `Bearer ${validToken}`)
+      .send(settingsData);
+
+    expect(responseSet.status).toBe(200);
+
+    const responseGet = await request(app)
+      .post('/api/v1/protected/getSettings')
+      .set('Authorization', `Bearer ${validToken}`)
+      .send({ player_id: registeredPlayerId });
+
+    expect(responseGet.status).toBe(200);
+    expect(responseGet.body).toEqual(settingsData);
+  });
+});
+
