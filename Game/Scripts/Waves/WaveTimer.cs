dafb417ae6df2371ec254c1d25c87bc9286e44ef
MarkoPiplica <markopiplica03@gmail.com>
2025-04-21 18:29:09 +0200

    added comments and commented out a common debug message

--- Game/Scripts/Waves/WaveTimer.cs
+++ Game/Scripts/Waves/WaveTimer.cs
@@ -11,29 +11,29 @@ public partial class WaveTimer : Node2D
 
 	public override void _Ready()
 	{
-		foreach (var player in GameManager.Players) {
+		foreach (var player in GameManager.Players) { // is for making the UI for the timer and wave counter only visible for one player and not for all players in the game
 			if (player.Id != Multiplayer.GetUniqueId()) {
 				GetNode<Label>("/root/Node2D/" + player.Id + "/Camera2D/TimeLeft").QueueFree();
 				GetNode<Label>("/root/Node2D/" + player.Id + "/Camera2D/WaveCounter").QueueFree();
 			}
 		}
-		Timer wave_timer = GetNode<Timer>("WaveTimer");
-		wave_timer.Timeout += OnTimerTimeout;
+		Timer wave_timer = GetNode<Timer>("WaveTimer"); 
+		wave_timer.Timeout += OnTimerTimeout; // calls method every second
 	}
 
 
 	private void OnTimerTimeout()
 	{
-		second_counter++;
+		second_counter++; // counts the seconds until the max_time is reached and a new wave begins
 		if (second_counter >= max_time)
 		{
-			IncreaseWaveCounter();
+			IncreaseWaveCounter(); // increases wave counter when the last wave is cleared 
 			if (GetTree().GetNodesInGroup("Enemies") != null)
 			{
 				Debug.Print("Starting enemy deletion");
-				foreach (Node2D enemy in GetTree().GetNodesInGroup("Enemies"))
+				foreach (Node2D enemy in GetTree().GetNodesInGroup("Enemies")) // deletes every enemy
 				{
-					Debug.Print("Deleted enemy");
+					//Debug.Print("Deleted enemy");
 					enemy.QueueFree();
 				}
 				second_counter = 0;
@@ -45,7 +45,7 @@ public partial class WaveTimer : Node2D
 		}
 		Label label = GetNode<Label>("/root/Node2D/" + Multiplayer.GetUniqueId() + "/Camera2D/TimeLeft");
 		var zeit = max_time - second_counter;
-		label.Text = zeit.ToString();
+		label.Text = zeit.ToString(); // changes text to the remaining time until the next wave
 	}
 
 	private void IncreaseWaveCounter()
