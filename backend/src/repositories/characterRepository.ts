390cfdd42fa618ac8867e551a6dcf642bd0e46b6
maxfriebertshauser <22frma1bif@hft-stuttgart.de>
2025-04-22 15:08:10 +0200

    feat(characters): refactor character insertion to clear existing data and read from JSON
    Co-authored-by: Paul <jaul.ponas@gmail.com>

--- backend/src/repositories/characterRepository.ts
+++ backend/src/repositories/characterRepository.ts
@@ -2,18 +2,13 @@ import * as fs from 'fs';
 import { AppDataSource } from '../libs/data-source';
 import { Character } from '../libs/entities/Character';
 
-export async function insertAllCharacters(): Promise<void> {
-  const jsonData = JSON.parse(fs.readFileSync('assets/characters/innitCharacters.json', 'utf-8'));
-  for (const character of jsonData.characters) {
-    const newCharacter = new Character();
-    newCharacter.characterId = character.characterId;
-    newCharacter.name = character.name;
-    newCharacter.speed = character.speed;
-    newCharacter.health = character.health;
-    newCharacter.dexterity = character.dexterity;
-    newCharacter.intelligence = character.intelligence;
-    await AppDataSource.getRepository(Character).save(newCharacter);
-  }
+async function deleteAll() {
+  await AppDataSource.getRepository(Character).clear();
+}
+
+export async function insertAllCharacters(chars: Character[]): Promise<void> {
+  await deleteAll();
+  await AppDataSource.getRepository(Character).save(chars);
 }
 export async function getAllCharactersRepo(): Promise<Character[]> {
   return await AppDataSource.getRepository(Character).find();
--- backend/src/services/characterService.ts
+++ backend/src/services/characterService.ts
@@ -1,10 +1,29 @@
 import { Character } from '../libs/entities/Character';
-import { insertAllCharacters,getAllCharactersRepo } from '../repositories/characterRepository';
+import { insertAllCharacters, getAllCharactersRepo } from '../repositories/characterRepository';
+import fs from 'fs';
 
 export async function innitAllCharacters(): Promise<void> {
-  await insertAllCharacters();
+  const chars = readCharacters();
+
+  await insertAllCharacters(chars);
 }
 
 export async function getAllCharacters(): Promise<void> {
   await getAllCharactersRepo();
+}
+
+function readCharacters(): Character[] {
+  const jsonData = JSON.parse(fs.readFileSync('assets/characters/innitCharacters.json', 'utf-8'));
+  const toInsertList = new Array<Character>();
+  for (const character of jsonData.characters) {
+    const newCharacter = new Character();
+    newCharacter.characterId = character.characterId;
+    newCharacter.name = character.name;
+    newCharacter.speed = character.speed;
+    newCharacter.health = character.health;
+    newCharacter.dexterity = character.dexterity;
+    newCharacter.intelligence = character.intelligence;
+    toInsertList.push(newCharacter);
+  }
+  return toInsertList;
 }
