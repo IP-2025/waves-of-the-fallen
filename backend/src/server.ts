b0b4b6a72c03464863046b74a486fb0060ef2753
Paul <jaul.ponas@gmail.com>
2025-04-24 13:40:01 +0200

    fixed init when starting
    Co-authored-by: maxfriebertshauser <22frma1bif@hft-stuttgart.de>

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import app from './app';
 import { PORT } from './core/config';
 import { AppDataSource } from './libs/data-source';
 import logger from './logger/logger';
+import { innitAllCharacters } from './services/characterService';
 
 const port = PORT || 3000;
 
@@ -10,6 +11,7 @@ AppDataSource.initialize()
   .then(() => {
     app.listen(port, () => {
       logger.info(`🚀 Server screaming on port ${port}`);
+      innitAllCharacters()
     });
   })
   .catch((error) => {
