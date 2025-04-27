6d27547962ba560c4c7f844037b15cc3eef03725
maxfriebertshauser <22frma1bif@hft-stuttgart.de>
2025-04-27 22:10:23 +0200

    feat(characters): add getAllUnlockedCharacterController to retrieve unlocked characters

--- backend/src/controllers/characterController.ts
+++ backend/src/controllers/characterController.ts
@@ -1,10 +1,19 @@
 import { RequestHandler } from 'express';
-import { getAllCharactersRepo } from '../repositories/characterRepository';
+import { getAllCharactersRepo, getAllUnlockedCharactersRepo } from '../repositories/characterRepository';
 import { getAllCharacters } from '../services/characterService';
 
 export const getAllCharacterController: RequestHandler = async (req, res) => {
   try {
     const characters = await getAllCharacters();
+    res.json(characters).status(200);
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
+};
+export const getAllUnlockedCharacterController: RequestHandler = async (req, res) => {
+  try {
+    const playerId = req.body.player_id;
+    const characters = await getAllUnlockedCharactersRepo(playerId);
     res.json(characters).status(200);
   } catch (err) {
     res.status(500).send('Server error');
