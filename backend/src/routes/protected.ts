f674520512066055382847424fce98b85362398f
maxfriebertshauser <22frma1bif@hft-stuttgart.de>
2025-04-27 22:10:37 +0200

    feat(characters): add endpoint to retrieve all unlocked characters

--- backend/src/routes/protected.ts
+++ backend/src/routes/protected.ts
@@ -2,13 +2,14 @@ import express from 'express';
 import { authenticationStep } from '../middleware/validateMiddleware';
 import { getSettings, setSettings } from '../controllers/settingsController';
 import {getAllCharacters} from '../services/characterService';
-import { getAllCharacterController } from '../controllers/characterController';
+import { getAllCharacterController, getAllUnlockedCharacterController } from '../controllers/characterController';
 
 const protectedRouter = express.Router();
 
 protectedRouter.post('/getSettings', authenticationStep, getSettings);
 protectedRouter.post('/setSettings', authenticationStep, setSettings);
 protectedRouter.post('/getAllCharacters',authenticationStep,getAllCharacterController);
+protectedRouter.post('/getAllUnlockedCharacters',authenticationStep,getAllUnlockedCharacterController);
 
 protectedRouter.get('/', authenticationStep, (req, res) => {
   res.json({ authenticated: true });
