1cec6fc0d30511ff20bad60180489fc30d296807
Pazl <jaul.ponas@gmail.com>
2025-04-28 15:39:25 +0200

    fixed small issue

---
+++ backend/dev/innit.sql
@@ -0,0 +1,6 @@
+INSERT INTO Player (player_id, username, gold)
+VALUES ('D465D2FD-092C-40A6-945D-E29E99FA524A
+', 'koch', 100);
+
+INSERT INTO UnlockedCharacter (player_id, characterId, level)
+VALUES ('D465D2FD-092C-40A6-945D-E29E99FA524A', 'char001', 1);
