b0ba430cae1650ed859b4a6425debab875310212
York <yorkkoch02@gmail.com>
2025-04-29 12:06:54 +0200

    chore(format): fix format

--- Game/Scripts/Menu/ButtonsCharacterSelection.cs
+++ Game/Scripts/Menu/ButtonsCharacterSelection.cs
@@ -5,33 +5,34 @@ public partial class ButtonsCharacterSelection : Button
 {
 	private Charactermenu _controller;
 	private Shader _blackAndWhiteShader;
-	
-	 public override void _Ready()
+
+	public override void _Ready()
 	{
-		if(_blackAndWhiteShader==null)
+		if (_blackAndWhiteShader == null)
 		{
-			_blackAndWhiteShader=GD.Load<Shader>("res://Scenes/Menu/characterMenuIconShader.gdshader");
+			_blackAndWhiteShader = GD.Load<Shader>("res://Scenes/Menu/characterMenuIconShader.gdshader");
 		}
-		Node node=GetParent();
-		while (_controller==null&&node!=null){
-			if(node is Charactermenu)
+		Node node = GetParent();
+		while (_controller == null && node != null)
+		{
+			if (node is Charactermenu)
 			{
-				_controller=(Charactermenu)node;
+				_controller = (Charactermenu)node;
 				break;
 			}
-			node=node.GetParent();
+			node = node.GetParent();
 		}
-		
-		if(this.Name=="Button_Character6"||this.Name=="Button_Character3") //if Character is locked =true
+
+		if (this.Name == "Button_Character6" || this.Name == "Button_Character3") //if Character is locked =true
 		{
 			var icon = GetNode<TextureRect>("TextureRect");
-			var material=new ShaderMaterial();
-			material.Shader=_blackAndWhiteShader;
-			icon.Material= material;
+			var material = new ShaderMaterial();
+			material.Shader = _blackAndWhiteShader;
+			icon.Material = material;
 		}
-		
+
 	}
-	
+
 	private void _on_pressed()
 	{
 		_controller._characterSelected(this);
