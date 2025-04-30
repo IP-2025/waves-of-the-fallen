cd80c077f8b66acc9e58937c2fd189bfa244037f
Paul <jaul.ponas@gmail.com>
2025-04-30 17:47:06 +0200

    fixed code so that tests work

--- backend/src/errors/notFoundError.ts
+++ backend/src/errors/notFoundError.ts
@@ -3,5 +3,6 @@ import { CustomError } from './customErorr';
 export class NotFoundError extends CustomError {
   constructor(message: string) {
     super(message, 404);
+    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
--- backend/src/services/loginService.ts
+++ backend/src/services/loginService.ts
@@ -1,26 +1,34 @@
 import { getPwdByMail, getPlayerIdFromCredential } from '../repositories/credentialsRepository';
-import { InternalServerError, UnauthorizedError } from '../errors';
+import { InternalServerError, NotFoundError, UnauthorizedError } from '../errors';
 import bcrypt from 'bcrypt';
 import { generateToken } from '../auth/jwt';
 
 export async function pwdCheck(email: string, password: string): Promise<string> {
   try {
     const credential = await getPwdByMail(email);
-
+    if (!credential) {
+      throw new UnauthorizedError('Credential not found for the given email.');
+    }
     const isMatch = await bcrypt.compare(password, credential.password);
     if (!isMatch) {
-      throw new UnauthorizedError('Wrong email or password.');
+      throw new UnauthorizedError('Invalid password.');
     }
 
-    const token = generateToken(credential.player.player_id);
+    const player_id = await getPlayerIdFromCredential(credential.id);
+    if (!player_id) {
+      throw new UnauthorizedError('Player ID not found for the given credential.');
+    }
+
+    // creating jwt
+    const token = generateToken(player_id);
     return token;
   } catch (error) {
     if (error instanceof UnauthorizedError) {
       throw error;
     }
-
-    console.error('Authentication error:', error);
-
-    throw new InternalServerError('An error occurred during authentication.');
+    if (error instanceof NotFoundError) {
+      throw new UnauthorizedError('Invalid email or password');
+    }
+    throw new InternalServerError('Internal server error');
   }
 }
