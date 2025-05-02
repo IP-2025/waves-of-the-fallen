243f79a8a103108809996951623739eee3d854b7
maxfriebertshauser <22frma1bif@hft-stuttgart.de>
2025-05-02 15:42:52 +0200

    feat(playerGoldStats): rename playerInfoController to playerStatController and add gold management endpoints

--- backend/src/controllers/playerStatController.ts
+++ backend/src/controllers/playerStatController.ts
@@ -1,7 +1,16 @@
-export async function getGoldController(){
+import { RequestHandler } from 'express';
+import { getAllUnlockedCharacters } from '../services/characterService';
 
 
-}
+export const getGoldController: RequestHandler = async (req, res) => {
+  try {
+    const playerId = req.body.player_id;
+    const characters = await getAllUnlockedCharacters(playerId);
+    res.json(characters).status(200);
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
+};
 
 export async function setGoldController(){
 
---
+++ backend/src/services/playerService.ts
@@ -0,0 +1,6 @@
+export async function getGoldService(id: string): Promise<void>{
+
+}
+export async function setGoldService(id: string): Promise<void>{
+
+}
