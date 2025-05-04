7261de145524d62f8436f99cd45a28f716a0d9ee
l3nny123 <120551935+l3nny123@users.noreply.github.com>
2025-05-04 14:23:24 +0200

    feat: reworked character menu
    reworked the highlighting of the currently pressed button

--- Game/Scripts/Menu/Charactermenu.cs
+++ Game/Scripts/Menu/Charactermenu.cs
@@ -86,8 +86,13 @@ public partial class Charactermenu : Control
 	{
 		if (button != null && _labelCharacterName != null)
 		{
+			SetButtonStyle(_currentlySelectedCharacter,Color.Color8(0x4F, 0x4F, 0x4F),false);
+			
 			SetCharacterPageValuesFromFile(button.Text);
 			_currentlySelectedCharacter = button;
+			
+			SetButtonStyle(_currentlySelectedCharacter,Color.Color8(0x2C, 0xC7, 0xFF),false);
+			
 			if (!characterManager.LoadIsUnlocked(button.Text))
 			{ //check if character is unlocken. locked=true
 				_ButtonUpgradeUnlock.Text = "Unlock";
@@ -100,6 +105,31 @@ public partial class Charactermenu : Control
 			}
 		}
 	}
+	
+	private void SetButtonStyle(Button button, Color color, bool addBorder){
+		var style = button.GetThemeStylebox("normal") as StyleBoxFlat;
+		if(style!=null){
+			
+			var newStyle=(StyleBoxFlat)style.Duplicate();
+			newStyle.BgColor=color;
+			newStyle.BorderColor = new Color(1f, 0f, 0f);
+			
+			if(addBorder){
+				newStyle.BorderColor = new Color(1f, 0f, 0f);
+				newStyle.SetBorderWidth(Side.Top, 3);
+				newStyle.SetBorderWidth(Side.Bottom, 3);
+				newStyle.SetBorderWidth(Side.Left, 3);
+				newStyle.SetBorderWidth(Side.Right, 3);
+			}
+		
+			
+			button.AddThemeStyleboxOverride("normal", newStyle);
+			button.AddThemeStyleboxOverride("hover", newStyle);
+			button.AddThemeStyleboxOverride("pressed", newStyle);
+			button.AddThemeStyleboxOverride("focus", newStyle);
+			
+		}
+	}
 
 	//wenn bei einem ausgewöhlten charcter der "select"- button gedrückt wird 
 	private void _on_button_select_pressed()
@@ -107,15 +137,8 @@ public partial class Charactermenu : Control
 		if (_currentlySelectedCharacter != null)
 		{
 			characterManager.SaveLastSelectedCharacterID(int.Parse(_currentlySelectedCharacter.Text));
-			var style = new StyleBoxFlat();
-			style.BgColor = Color.Color8(0x4F, 0x4F, 0x4F);
-			style.BorderColor = new Color(1f, 0f, 0f);
-			style.SetBorderWidth(Side.Top, 3);
-			style.SetBorderWidth(Side.Bottom, 3);
-			style.SetBorderWidth(Side.Left, 3);
-			style.SetBorderWidth(Side.Right, 3);
-			_currentlySelectedCharacter.AddThemeStyleboxOverride("normal", style);
-
+			SetButtonStyle(_currentlySelectedCharacter, Color.Color8(0x2C, 0xC7, 0xFF),true);
+			
 			if (_oldSelectedCharacter != _currentlySelectedCharacter)
 			{
 				_resetButton(_oldSelectedCharacter);
@@ -123,6 +146,8 @@ public partial class Charactermenu : Control
 			}
 		}
 	}
+	
+	// temp function for temp reset button to reset the character data 
 	private void _resetButton(Button button)
 	{
 		if (button != null)
@@ -130,6 +155,9 @@ public partial class Charactermenu : Control
 			var style = new StyleBoxFlat();
 			style.BgColor = Color.Color8(0x4F, 0x4F, 0x4F);
 			button.AddThemeStyleboxOverride("normal", style);
+			button.AddThemeStyleboxOverride("hover", style);
+			button.AddThemeStyleboxOverride("pressed", style);
+			button.AddThemeStyleboxOverride("focus", style);
 		}
 	}
 	
