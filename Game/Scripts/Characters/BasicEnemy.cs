44ae56e80091ef4ea23abf72bc630f03d457c5d2
Manu <22homa1bif@hft-stuttgart.de>
2025-05-07 14:13:24 +0200

    fixed spritesheet and integrated walk animation

--- Game/Scripts/Characters/BasicEnemy.cs
+++ Game/Scripts/Characters/BasicEnemy.cs
@@ -4,6 +4,14 @@ using System.Diagnostics;
 
 public partial class BasicEnemy : EnemyBase
 {
+	private AnimatedSprite2D animation;
+	
+	public override void _Ready()
+	{
+		base._Ready();
+		animation = GetNode<AnimatedSprite2D>("GoblinAnimation");
+	}
+	
 	public override void _PhysicsProcess(double delta)
 	{
 		FindNearestPlayer();
@@ -12,6 +20,15 @@ public partial class BasicEnemy : EnemyBase
 			LookAt(player.GlobalPosition);
 			Vector2 direction = (player.GlobalPosition - GlobalPosition).Normalized();
 			Velocity = direction * speed;
+			
+			if (Velocity.Length() > 0.1f)
+			{
+				PlayIfNotPlaying("walk");
+			}
+			else
+			{
+				PlayIfNotPlaying("idle");
+			}
 		}
 		else
 		{
@@ -21,10 +38,17 @@ public partial class BasicEnemy : EnemyBase
 		MoveAndSlide();
 	}
 
-
 	public override void Attack() 
 	{
 		player.GetNode<Health>("Health").Damage(damage);
 		Debug.Print("BasicEnemy attacks (melee)!");
+	}
+	
+	private void PlayIfNotPlaying(string animName)
+	{
+		if (animation.Animation != animName)
+		{
+			animation.Play(animName);
+		}	
 	}
 }
