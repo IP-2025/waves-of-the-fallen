2c8315b255758ba83c1536cd38845c264e33e596
PizzaE4ter <12ibbi1bif@hft-stuttgart.de>
2025-05-12 15:20:06 +0200

    Add summary to RangedEnemy and define standard values in constructor

--- Game/Scripts/Characters/RangedEnemy.cs
+++ Game/Scripts/Characters/RangedEnemy.cs
@@ -1,5 +1,11 @@
 using Godot;
 
+/// <summary>
+/// RangedEnemy configuration:
+/// - Standard values (speed, damage, attacksPerSecond) are defined in the constructor.
+/// - stopDistance: Distance at which the RangedEnemy stops moving towards the player.
+/// - attackRange: Maximum range within which the RangedEnemy can attack.
+/// </summary>
 public partial class RangedEnemy : EnemyBase
 {
 	[Export] public float stopDistance = 350f;
