2b26fcf1b03b0e74bf802f3888fe34f3e1967df8
22xija1bif <22xija1bif@hft-stuttgart.de>
2025-05-13 21:06:32 +0200

    Update DefaultPlayer.cs

--- Game/Scripts/Characters/DefaultPlayer.cs
+++ Game/Scripts/Characters/DefaultPlayer.cs
@@ -127,6 +127,13 @@ public partial class DefaultPlayer : CharacterBody2D
 		
 		if (playerClass is Assassin)
 			return KunaiScene.Instantiate() as Area2D;
+<<<<<<< Updated upstream
+=======
+		if (playerClass is Knight)
+			//return DaggerScene.Instantiate() as Area2D;
+			return SwordScene.Instantiate() as Area2D;
+		// if (playerClass is Mage) return FireStaffScene.Instantiate() as Area2D;
+>>>>>>> Stashed changes
 
 		if (playerClass is Mage) 
 			return FireStaffScene.Instantiate() as Area2D;
