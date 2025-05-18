ca2eb22b2b680fc49fcb67830a5229b8e106a3f5
22xija1bif <22xija1bif@hft-stuttgart.de>
2025-05-18 19:08:54 +0200

    Update Dagger.cs
    deleted comment

--- Game/Scripts/Weapons/MeleeWeapons/Dagger.cs
+++ Game/Scripts/Weapons/MeleeWeapons/Dagger.cs
@@ -8,18 +8,8 @@ public partial class Dagger : MeleeWeapon
 	{
 		animatedSprite = GetNode<AnimatedSprite2D>("./WeaponPivot/Stab");
 		MeleeDamage = 50;
-		//Standard Dagger Animation or use nothing.
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	/*protected override void MeleeAttack(Node target)
-	{
-		animatedSprite.Play("stab");
-		if (target.HasMethod("TakeDamage"))
-		{
-			target.Call("TakeDamage", Damage);
-		}
-	}*/
 	public async void OnTimerTimeoutDagger()
 	{
 		animatedSprite.Play("Stab");
