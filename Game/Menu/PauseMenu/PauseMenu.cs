ad22200296fe711ebafcfa4c5bc6403bc15a45b4
PizzaE4ter <12ibbi1bif@hft-stuttgart.de>
2025-06-08 18:18:49 +0200

    main menu doesnt freeze anymore if quitted within game

--- Game/Menu/PauseMenu/PauseMenu.cs
+++ Game/Menu/PauseMenu/PauseMenu.cs
@@ -3,51 +3,52 @@ using Game.Utilities.Multiplayer;
 
 public partial class PauseMenu : Control
 {
-    public override void _Ready()
-    {
-        ProcessMode = ProcessModeEnum.Always; // important to continue while paused
-        GetNode<Button>("Background/VBoxContainer/Resume").Pressed += OnResumePressed;
-        GetNode<Button>("Background/VBoxContainer/Settings").Pressed += OnSettingsPressed;
-        GetNode<Button>("Background/VBoxContainer/Main Menu").Pressed += OnMainMenuPressed;
-        GetNode<Button>("Background/VBoxContainer/Quit").Pressed += OnQuitPressed;
-        Visible = false;
-    }
+	public override void _Ready()
+	{
+		ProcessMode = ProcessModeEnum.Always; // important to continue while paused
+		GetNode<Button>("Background/VBoxContainer/Resume").Pressed += OnResumePressed;
+		GetNode<Button>("Background/VBoxContainer/Settings").Pressed += OnSettingsPressed;
+		GetNode<Button>("Background/VBoxContainer/Main Menu").Pressed += OnMainMenuPressed;
+		GetNode<Button>("Background/VBoxContainer/Quit").Pressed += OnQuitPressed;
+		Visible = false;
+	}
 
-    private void OnResumePressed()
-    {
-        Visible = false;
-    
-        if (NetworkManager.Instance._soloMode)
-            GetTree().Paused = false;
-    }
+	private void OnResumePressed()
+	{
+		Visible = false;
+	
+		if (NetworkManager.Instance._soloMode)
+			GetTree().Paused = false;
+	}
 
-    private void OnSettingsPressed()
-    {
-        var settingsScene = GD.Load<PackedScene>("res://Menu/Settings/settingsMenu.tscn");
-        var settingsMenu = settingsScene.Instantiate();
-        GetTree().Root.AddChild(settingsMenu);
-        Visible = false;
-    }
+	private void OnSettingsPressed()
+	{
+		var settingsScene = GD.Load<PackedScene>("res://Menu/Settings/settingsMenu.tscn");
+		var settingsMenu = settingsScene.Instantiate();
+		GetTree().Root.AddChild(settingsMenu);
+		Visible = false;
+	}
 
-    private void OnMainMenuPressed()
-    {
-        GetTree().ChangeSceneToFile("res://Menu/Main/mainMenu.tscn");
-    }
+	private void OnMainMenuPressed()
+	{
+		GetTree().Paused = false;
+		GetTree().ChangeSceneToFile("res://Menu/Main/mainMenu.tscn");
+	}
 
-    private void OnQuitPressed()
-    {
-        GetTree().Quit();
-    }
+	private void OnQuitPressed()
+	{
+		GetTree().Quit();
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        if (Visible && @event.IsActionPressed("ui_cancel"))
-        {
-            Visible = false;
-            if (NetworkManager.Instance._soloMode)
-                GetTree().Paused = false;
-            // waste input event to prevent it from propagating further
-            @event.Dispose();
-        }
-    }
+	public override void _Input(InputEvent @event)
+	{
+		if (Visible && @event.IsActionPressed("ui_cancel"))
+		{
+			Visible = false;
+			if (NetworkManager.Instance._soloMode)
+				GetTree().Paused = false;
+			// waste input event to prevent it from propagating further
+			@event.Dispose();
+		}
+	}
 }
