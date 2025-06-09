12b2a3053f6417c3712888fee0ef9a99abbae2fc
l3nny123 <120551935+l3nny123@users.noreply.github.com>
2025-06-09 10:53:20 +0200

    fix: audio

--- Game/Menu/Credits/CreditsPage.cs
+++ Game/Menu/Credits/CreditsPage.cs
@@ -7,7 +7,7 @@ public partial class CreditsPage : Control
     private void _on_button_BackToMainMenu_pressed()
     {
         var scene = ResourceLoader.Load<PackedScene>("res://Menu/Main/mainMenu.tscn");
-        SoundManager.Instance.PlayUI();
+        SoundManager.Instance.PlaySound(SoundManager.Instance.GetNode<AudioStreamPlayer>("buttonPress"));
         GetTree().ChangeSceneToPacked(scene);
     }
 }
