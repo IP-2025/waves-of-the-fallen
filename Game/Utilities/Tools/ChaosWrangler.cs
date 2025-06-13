ba5426c37e28cf8326191e1acc0316fa2b4e3f57
Kaprele <kaprele@gmx.de>
2025-06-13 19:19:11 +0200

    feat: :sparkles: keine ahnung warum jetzt aber es funktioniert

---
+++ Game/Utilities/Tools/ChaosWrangler.cs
@@ -0,0 +1,22 @@
+﻿using Godot;
+using System;
+using Game.Utilities.Multiplayer;
+
+public partial class ChaosWrangler : Node
+{
+    public static ChaosWrangler Instance { get; private set; }
+
+    public override void _Ready()
+    {
+        Instance = this;
+    }
+
+    public void StartGame()
+    {
+        var characterManager = GetNode<CharacterManager>("/root/CharacterManager");
+        int selectedCharacterId = characterManager.LoadLastSelectedCharacterID();
+        NetworkManager.Instance.RpcId(1, "SelectCharacter", selectedCharacterId);
+        NetworkManager.Instance.Rpc("NotifyGameStart");
+        // SoundManager.Instance.PlayUI();
+    }
+}
