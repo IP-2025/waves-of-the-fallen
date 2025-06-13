66f9dd183300097a6dfc29d3958132a3574033a4
MarkoPiplica <markopiplica03@gmail.com>
2025-06-13 15:55:46 +0200

    ArrowRain done

--- Game/UI/Ability/Ablities/ArrowRain.cs
+++ Game/UI/Ability/Ablities/ArrowRain.cs
@@ -3,22 +3,33 @@ using System;
 
 public partial class ArrowRain : AbilityBase
 {
-	private const string _resBase = "res://Weapons/Ranged/MagicStaffs/Firestaff/";
-	private const string _projectilePath = _resBase + "fireball.tscn";
-	public float DefaultRange { get; set; } = 1000f;
-	public int DefaultDamage { get; set; } = 500;
-	public int DefaultPiercing { get; set; } = FireBall.DefaultPiercing;
-	public float DefaultSpeed { get; set; } = 300;
+	private const string _resBase = "res://Weapons/Ranged/Bow/";
+	private const string _projectilePath = _resBase + "bow_arrow.tscn";
+	public float DefaultRange { get; set; } = 600f;
+	public int DefaultDamage { get; set; } = BowArrow.DefaultDamage;
+	public int DefaultPiercing { get; set; } = 1;
+	public float DefaultSpeed { get; set; } = 1300;
+	public int CountArrows { get; set; } = 30;
 	private int cooldown = 30;
 	private static readonly PackedScene _fireballPacked = GD.Load<PackedScene>(_projectilePath);
 
 	public override void _Ready()
 	{
-		
-		var target = FindNearestEnemy();
-		if (target != null && TryGetPosition(target, out var position))
-			LookAt(position);
-		Shoot();      
+		DoArrowRain(); 
+	}
+
+	public async void DoArrowRain()
+	{
+		Random random = new Random();
+		for (int i = 0; i < CountArrows; i++)
+		{
+			Vector2 vector1 = new Vector2(random.Next(-60, 60), random.Next(-60, 60));
+			var target = FindNearestEnemy();
+			if (target != null && TryGetPosition(target, out var position))
+				LookAt(position + vector1);
+			await ToSignal(GetTree().CreateTimer(0.1), "timeout");
+			Shoot();
+		}
 	}
 	
 	protected Node FindNearestEnemy()
