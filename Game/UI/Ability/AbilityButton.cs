2cf3d712ff08a68d77be113b37db5e6bf083a057
MarkoPiplica <markopiplica03@gmail.com>
2025-06-16 03:41:51 +0200

    Added the structure for having a picture displayed for the abilitybutton

--- Game/UI/Ability/AbilityButton.cs
+++ Game/UI/Ability/AbilityButton.cs
@@ -1,6 +1,7 @@
 using Godot;
 using Godot.NativeInterop;
 using System;
+using System.ComponentModel;
 using System.Diagnostics;
 
 public partial class AbilityButton : Node2D
@@ -12,7 +13,9 @@ public partial class AbilityButton : Node2D
 	private Node2D _parent;
 	private Timer _abilityTimer;
 	private int _abilityIndex;
-	private Sprite2D _abilityPic;
+	private TextureRect _abilityReadyPic;
+	private TextureRect _abilityNotReadyPic;
+	private TouchScreenButton _abilityButton;
 	private Label _timeLeftLabel;
 	private CharacterManager _characterManager;
 	private AbilityBase abilityBase;
@@ -24,12 +27,18 @@ public partial class AbilityButton : Node2D
 	private FireBlast fireBlast = new FireBlast();
 	private ArrowRain arrowRain = new ArrowRain();
 	private DeadlyStrike deadlyStrike = new DeadlyStrike();
+	private string textureActive = "res://AppIcon/AbilityIcons/PNG/Activated/BoostDexterityIcon.png";
+	private string textureNotActive = "res://AppIcon/AbilityIcons/PNG/Activated/BoostDexterityIcon.png";
 
 	public override void _Ready()
 	{
 		_timeLeftLabel = GetNode<Label>("TimeLeft");
-		_abilityPic = GetNode<Sprite2D>("%AbilityPic");
+		_abilityReadyPic = GetNode<TextureRect>("%AbilityReadyPic");
+		_abilityNotReadyPic = GetNode<TextureRect>("%AbilityNotReadyPic");
 		_parent = GetParent<Node2D>();
+		_abilityButton = GetNodeOrNull<TouchScreenButton>("%TouchAbilityButton");
+		Debug.Print(_abilityButton.GetInstanceId().ToString());
+		//GetNode<TouchScreenButton>("TouchAbilityButton");
 		_characterManager = GetNode<CharacterManager>("/root/CharacterManager");
 		var selectedId = _characterManager.LoadLastSelectedCharacterID();
 
@@ -37,6 +46,7 @@ public partial class AbilityButton : Node2D
 		{
 			_abilityIndex = selectedId * 10 + _characterManager.LoadAbilityChosenByID(selectedId.ToString());
 
+			//I am sorry for this code
 			MaxTime = _abilityIndex switch
 			{
 				11 => boostDexterity.getCooldown(),
@@ -49,10 +59,38 @@ public partial class AbilityButton : Node2D
 				42 => fireBlast.getCooldown(),
 				_ => 0
 			};
+			textureActive = _abilityIndex switch
+			{
+				11 => "res://AppIcon/AbilityIcons/PNG/Activated/BoostDexterityIcon.png",
+				12 => "res://AppIcon/AbilityIcons/PNG/Activated/ArrowRainIcon.png",
+				21 => "res://AppIcon/AbilityIcons/PNG/Activated/DashIcon.png",
+				22 => "res://AppIcon/AbilityIcons/PNG/Activated/DeadlyStrikeIcon.png",
+				31 => "res://AppIcon/AbilityIcons/PNG/Activated/BoostStrengthIcon.png",
+				32 => "res://AppIcon/AbilityIcons/PNG/Activated/ShieldIcon.png",
+				41 => "res://AppIcon/AbilityIcons/PNG/Activated/BoostIntelligenceIcon.png",
+				42 => "res://AppIcon/AbilityIcons/PNG/Activated/FireBlastIcon.png",
+				_ => ""
+			};
+			textureNotActive = _abilityIndex switch
+			{
+				11 => "res://AppIcon/AbilityIcons/PNG/Deactivated/BoostDexterityIcon.png",
+				12 => "res://AppIcon/AbilityIcons/PNG/Deactivated/ArrowRainIcon.png",
+				21 => "res://AppIcon/AbilityIcons/PNG/Deactivated/DashIcon.png",
+				22 => "res://AppIcon/AbilityIcons/PNG/Deactivated/DeadlyStrikeIcon.png",
+				31 => "res://AppIcon/AbilityIcons/PNG/Deactivated/BoostStrengthIcon.png",
+				32 => "res://AppIcon/AbilityIcons/PNG/Deactivated/ShieldIcon.png",
+				41 => "res://AppIcon/AbilityIcons/PNG/Deactivated/BoostIntelligenceIcon.png",
+				42 => "res://AppIcon/AbilityIcons/PNG/Deactivated/FireBlastIcon.png",
+				_ => ""
+			};
 		}
+		Debug.Print(textureActive);
+		Debug.Print(textureNotActive);
 
 		_timeLeftLabel.Text = MaxTime.ToString();
 		_abilityTimer = GetNode<Timer>("AbilityTimer");
+		_abilityReadyPic.Texture.TakeOverPath(textureActive);
+		_abilityNotReadyPic.Texture.TakeOverPath(textureNotActive);
 		_abilityTimer.Timeout += OnTimerTimeout;
 		SecondCounter = MaxTime;
 		Disable = true;
@@ -67,7 +105,8 @@ public partial class AbilityButton : Node2D
 			_abilityTimer.Stop();
 			_timeLeftLabel.Hide();
 			Disable = false;
-			//Pic for ability ready show insert here
+			_abilityReadyPic.Show();
+			_abilityNotReadyPic.Hide();
 		}
 		_timeLeftLabel.Text = (MaxTime - SecondCounter).ToString();
 		IsPaused = _abilityTimer.Paused;
@@ -84,6 +123,8 @@ public partial class AbilityButton : Node2D
 				player.UseAbility();
 				//Debug.Print(MaxTime.ToString());
 			}
+			_abilityReadyPic.Hide();
+			_abilityNotReadyPic.Show();
 			_abilityTimer.Start();
 			_timeLeftLabel.Show();
 		}
