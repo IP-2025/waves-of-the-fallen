9156da6049ba7ce7fb3221fb5c2f8a5df6a3edbf
Pazl <130466427+Pazl27@users.noreply.github.com>
2025-06-19 21:58:54 +0200

    should send request to actual server

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/v1': {
-        target: 'http://localhost:8080',
+        target: 'http://waves-of-the-fallen.duckdns.org:32424',
         ws: true,
         changeOrigin: true,
         rewrite: (path) => path
